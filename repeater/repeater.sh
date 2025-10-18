cafa0bf991250efcff77954b9065fcabdd5c9c65
Akinori Furuta <afuruta@m7.dion.ne.jp>
2025-10-18 15:55:21 +0900

    Add script to retry until success to execute command

---
+++ repeater/repeater.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-2-Clause
+
+result=0
+
+time_marker=$( date +%y%m%d%H%M%S )
+cmd_body=${1##*/}
+cmd_body=${cmd_body%.*}
+prefix="repeater_${cmd_body}_${time_marker}_"
+i=0
+while (( ${i} <= 9999 ))
+do
+	echo "$0: Try. i=${i}"
+
+	now_date=$( date +%y%m%d%H%M%S )
+	log_file="${prefix}$( printf "%04d" ${i} )_${now_date}.log"
+	if "$@"  2>&1 | tee "${log_file}"
+	result=$?
+	then
+		echo "$0: Success. i=${i}"
+		break
+	fi
+	i=$(( ${i} + 1 ))
+done
+exit ${result}
